b083bd3e0dcb582d04e97e5bd87bc70a6d305d6c
Somsekhar Dash <somsekhardash@gmail.com>
2021-04-01 01:20:02 +0530

    build fix

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: "/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
